8689a1d78df09abc814ff3543472037596ed3c51
Onakice <pongsapak.tatongjai+1@gmail.com>
2025-09-15 21:26:09 +0700

    fix case have 2 argv

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
-for i in range(11):
-    print(f"Table de {i}: ", end="")
-    for j in range(11):
-        print(f"{i*j}", end=" ")
-    print()
+import sys
+
+i = 0
+if len(sys.argv) > 1:
+    print("none")
+else :
+    while i <= 10 :
+        print(f"Table de {i}: ", end="")
+        j = 0
+        while j <= 10:
+            print(f"{i*j}", end=" ")
+            j+=1
+        print()
+        i+=1
